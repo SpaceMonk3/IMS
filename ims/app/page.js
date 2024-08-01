475166ed6ba306697fb21693cf3ab03d22624dfb
Dheer Tammina <dtammina@gmu.edu>
2024-08-01 18:18:24 +0530

    updating and deleting items in firebase

--- ims/app/page.js
+++ ims/app/page.js
@@ -47,6 +47,32 @@ export default function Home() {
   useEffect(() => {
     updateInventory()
   }, [])
+
+  const addItem = async (item) => {
+    const docRef = doc(collection(firestore, 'inventory'), item)
+    const docSnap = await getDoc(docRef)
+    if (docSnap.exists()) {
+      const { quantity } = docSnap.data()
+      await setDoc(docRef, { quantity: quantity + 1 })
+    } else {
+      await setDoc(docRef, { quantity: 1 })
+    }
+    await updateInventory()
+  }
+  
+  const removeItem = async (item) => {
+    const docRef = doc(collection(firestore, 'inventory'), item)
+    const docSnap = await getDoc(docRef)
+    if (docSnap.exists()) {
+      const { quantity } = docSnap.data()
+      if (quantity === 1) {
+        await deleteDoc(docRef)
+      } else {
+        await setDoc(docRef, { quantity: quantity - 1 })
+      }
+    }
+    await updateInventory()
+  }
   
   return (
     <Box>
